08184ff17178bd6f4bc0724be9680ed99aaa0f5e
Dmytro Yurchenko <dym@box4.me>
2024-07-14 00:12:43 +0200

    fixes

--- casa/run_once_02__install_fnm.sh
+++ casa/run_once_02__install_fnm.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
 if [[ "$(type fnm)" == "fnm not found" ]]; then
-	curl -fsSL https://fnm.vercel.app/install | zsh /dev/stdin --skip-shell
-	source $HOME/.zshrc
+	curl -fsSL https://fnm.vercel.app/install | zsh /dev/stdin --skip-shell --install-dir "$HOME/.fnm"
+	source ${HOME}/.zshrc
 	fnm install v20.14.0
 else
 	echo "fnm already installed"
