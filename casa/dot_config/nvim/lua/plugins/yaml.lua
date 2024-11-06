9f21f832ee753fc00378a1e7dd5e440798afec98
Dmytro Yurchenko <dmytro.yurchenko@swissmarketplace.group>
2024-11-06 16:28:08 +0100

    update lazyvim plugins

--- casa/dot_config/nvim/lua/plugins/yaml.lua
+++ casa/dot_config/nvim/lua/plugins/yaml.lua
@@ -1,18 +1,40 @@
 return {
-  --   {
-  --     "neovim/nvim-lspconfig",
-  --     config = function()
-  --       require("lspconfig").yamlls.setup({
-  --         yaml = {
-  --           editor = {
-  --             tabSize = 2,
-  --           },
-  --           format = {
-  --             singleQuote = true,
-  --           },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     require("lspconfig").yamlls.setup({
+  --       yaml = {
+  --         editor = {
+  --           tabSize = 2,
   --         },
-  --       })
-  --       -- Configuration here, or leave empty to use defaults)
-  --     end,
-  --   },
+  --         format = {
+  --           singleQuote = true,
+  --         },
+  --         hover = true,
+  --         completion = true,
+  --         customTags = {
+  --           "!fn",
+  --           "!And",
+  --           "!If",
+  --           "!Not",
+  --           "!Equals",
+  --           "!Or",
+  --           "!FindInMap sequence",
+  --           "!Base64",
+  --           "!Cidr",
+  --           "!Ref",
+  --           "!Ref Scalar",
+  --           "!Sub",
+  --           "!GetAtt",
+  --           "!GetAZs",
+  --           "!ImportValue",
+  --           "!Select",
+  --           "!Split",
+  --           "!Join sequence",
+  --         },
+  --       },
+  --     })
+  --     -- Configuration here, or leave empty to use defaults)
+  --   end,
+  -- },
 }
