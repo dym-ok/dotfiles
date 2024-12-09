f5bc1add9e51dc0a40ba8f079e2653a4694be23a
Dmytro Yurchenko <dmytro.yurchenko@swissmarketplace.group>
2024-12-09 14:29:13 +0100

    Jupyter notebook support

--- casa/dot_config/nvim/lua/config/keymaps.lua
+++ casa/dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+-- set a keybinding for the AiderOpen function
+map("n", "<leader>oa", "<cmd>lua AiderOpen()<cr>", { noremap = true, silent = true })
+-- set a keybinding for the AiderBackground function
+map("n", "<leader>ob", "<cmd>lua AiderBackground()<cr>", { noremap = true, silent = true })
---
+++ casa/dot_config/nvim/lua/plugins/notebooks.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "benlubas/molten-nvim",
+    version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+    build = ":UpdateRemotePlugins",
+    dependencies = { "3rd/image.nvim" },
+    init = function()
+      -- this is an example, not a default. Please see the readme for more configuration options
+      vim.g.molten_image_provider = "image.nvim"
+      vim.g.molten_output_win_max_height = 20
+    end,
+  },
+  {
+    "GCBallesteros/NotebookNavigator.nvim",
+    keys = {
+      {
+        "]h",
+        function()
+          require("notebook-navigator").move_cell("d")
+        end,
+      },
+      {
+        "[h",
+        function()
+          require("notebook-navigator").move_cell("u")
+        end,
+      },
+      { "<leader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
+      { "<leader>x", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
+    },
+    dependencies = {
+      "echasnovski/mini.comment",
+      -- "hkupty/iron.nvim", -- repl provider
+      -- "akinsho/toggleterm.nvim", -- alternative repl provider
+      "benlubas/molten-nvim", -- alternative repl provider
+      "anuvyklack/hydra.nvim",
+    },
+    event = "VeryLazy",
+    config = function()
+      local nn = require("notebook-navigator")
+      nn.setup({ activate_hydra_keys = "<leader>h" })
+    end,
+  },
+}
