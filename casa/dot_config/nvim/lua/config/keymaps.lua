4850717434297f9593a222705224a88718cf541a
Dmytro Yurchenko <dmytro.yurchenko@swissmarketplace.group>
2025-04-15 15:21:47 +0200

    keymaps

--- casa/dot_config/nvim/lua/config/keymaps.lua
+++ casa/dot_config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,15 @@ local map = LazyVim.safe_keymap_set
 map("n", "<leader>oa", "<cmd>lua AiderOpen()<cr>", { noremap = true, silent = true })
 -- set a keybinding for the AiderBackground function
 map("n", "<leader>ob", "<cmd>lua AiderBackground()<cr>", { noremap = true, silent = true })
+map("v", "p", '"_dP', { noremap = true, silent = true, desc = "Paste without overwriting register" })
+map({ "n", "v" }, "d", '"_d', { desc = "Delete without saving to buffer" })
+map("n", "D", '"_D', { desc = "Delete line without saving to buffer" })
+map("n", "x", '"_x', { desc = "Delete character without saving to buffer" })
+
+return {
+  keys = {
+    { "d", '"_d', mode = { "n", "v" }, desc = "Delete without saving to buffer" },
+    { "D", '"_D', mode = "n", desc = "Delete line without saving to buffer" },
+    { "x", '"_x', mode = "n", desc = "Delete character without saving to buffer" },
+  },
+}
