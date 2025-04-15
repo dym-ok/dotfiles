4bff24b673d2612c3cd981daa8a7fb5639790b17
Dmytro Yurchenko <dmytro.yurchenko@swissmarketplace.group>
2025-04-15 18:13:17 +0200

    updates

--- casa/run_once_01_install_micromamba.sh
+++ casa/run_once_01_install_micromamba.sh
@@ -2,7 +2,24 @@
 if [[ "$(type micromamba)" == "micromamba not found" ]]; then
 	echo "installing micromamba"
 	"${SHELL}" <(curl -L micro.mamba.pm/install.sh)
-	micromamba shell init -s zsh -p $HOME/micromamba
+	micromamba shell init -s zsh -p "$HOME/micromamba"
 else
 	echo "micromamba already installed"
+fi
+
+if [[ "$(type poetry)" == "poetry not found" ]]; then
+	echo "installing poetry"
+	pipx install poetry
+else
+	echo "poetry already installed"
+fi
+
+if [[ "$(type poe)" == "poe not found" ]]; then
+	echo "installing poe"
+	pipx install poethepoet
+	mkdir -p ~/.oh-my-zsh/completions
+	poe _zsh_completion >~/.oh-my-zsh/completions/_poe
+
+else
+	echo "poe already installed"
 fi
