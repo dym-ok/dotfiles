61f9ef1a3b2b7036c01e3e2342a9409db585e674
Dmytro Yurchenko <dym@box4.me>
2025-05-11 18:48:13 +0200

    feat: add Arch Linux support

--- casa/run_once_02__install_fnm.sh
+++ casa/run_once_02__install_fnm.sh
@@ -2,7 +2,7 @@
 if [[ "$(type fnm)" == "fnm not found" ]]; then
 	curl -fsSL https://fnm.vercel.app/install | zsh /dev/stdin --skip-shell --install-dir "$HOME/.fnm"
 	source ${HOME}/.zshrc
-	fnm install v20.14.0
+	fnm install v22.15.0
 else
 	echo "fnm already installed"
 fi
