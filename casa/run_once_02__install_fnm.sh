b2c815b1c11251af3397d002a2ca82638c6b0269
Dmytro Yurchenko <dmytro.yurchenko@swissmarketplace.group>
2026-05-09 12:43:37 +0200

    update node to v24

--- casa/run_once_02__install_fnm.sh
+++ casa/run_once_02__install_fnm.sh
@@ -2,7 +2,7 @@
 if [[ "$(type fnm)" == "fnm not found" ]]; then
 	curl -fsSL https://fnm.vercel.app/install | zsh /dev/stdin --skip-shell --install-dir "$HOME/.fnm"
 	source ${HOME}/.zshrc
-	fnm install v22.15.0
+	fnm install v24.15.0
 else
 	echo "fnm already installed"
 fi
